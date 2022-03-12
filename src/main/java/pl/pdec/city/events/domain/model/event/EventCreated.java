01c273b642dae82fa538d0b2c1cd75b54bb2e1b5
pdec86 <pdec86dev@gmail.com>
2022-03-12 07:41:16 +0100

    Events

--- src/main/java/pl/pdec/city/events/domain/model/event/EventCreated.java
+++ src/main/java/pl/pdec/city/events/domain/model/event/EventCreated.java
@@ -12,43 +12,35 @@ import pl.pdec.city.utils.CityDebugger;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
-import java.util.HashSet;
-import java.util.Locale;
 import java.util.UUID;
 
 public class EventCreated extends AbstractEvent {
 
-    @SuppressWarnings("FieldMayBeFinal")
+    @SuppressWarnings({"NotNullFieldNotInitialized"})
     @NonNull
     private UUID eventId;
 
-    @SuppressWarnings("FieldMayBeFinal")
+    @SuppressWarnings({"NotNullFieldNotInitialized"})
     @NonNull
     private String name;
 
-    @SuppressWarnings("FieldMayBeFinal")
+    @SuppressWarnings({"NotNullFieldNotInitialized"})
     @NonNull
     private Calendar startDateTime;
 
-    @SuppressWarnings("FieldMayBeFinal")
+    @SuppressWarnings({"NotNullFieldNotInitialized"})
     @NonNull
     private Calendar endDateTime;
 
-    @SuppressWarnings("FieldMayBeFinal")
+    @SuppressWarnings({"NotNullFieldNotInitialized"})
     @NonNull
     private User owner;
 
-    @SuppressWarnings("FieldMayBeFinal")
+    @SuppressWarnings({"NotNullFieldNotInitialized"})
     @NonNull
     private BigDecimal totalPrice;
 
     protected EventCreated() {
-        this.eventId = UUID.randomUUID();
-        this.name = "";
-        this.startDateTime = Calendar.getInstance(Locale.getDefault());
-        this.endDateTime = Calendar.getInstance(Locale.getDefault());
-        this.owner = new User("anonymous", "empty_pass", null, new HashSet<>());
-        this.totalPrice = new BigDecimal("");
     }
 
     public EventCreated(@NonNull UUID eventId, @NonNull String name, @NonNull Calendar startDateTime,
